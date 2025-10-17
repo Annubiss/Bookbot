f0d707cc262602d8f3792db202294d8c063809d7
Annubiss <adilmadra9@gmail.com>
2025-10-17 17:25:22 +0300

    e

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+
+def get_book_text(book_id):
+    with open(book_id) as f:
+         file_contents = f.read()
+         return file_contents
+    
+
+def main():
+     get_book_text("home/adil/Bookbot/books/frankenstein.txt")
+
+
+
+main()
+
