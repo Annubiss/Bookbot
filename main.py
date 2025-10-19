cc4590d6b747f2bc92753da006da08526eae32cd
Annubiss <adilmadra9@gmail.com>
2025-10-19 14:42:30 +0300

    E: added dict count

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
+from stats import count_words
+from stats import count_characters
 def get_book_text(book_id):
     with open(book_id) as f:
          return f.read()
          
     
-
-def count_words(text):
-     return len(text.split())
      
 
 
 def main():
      text = get_book_text("/home/adil/Bookbot/books/frankenstein.txt")
      print(f"Found {count_words(text)} total words")
+     print(count_characters(text))
 
 
 
--- stats.py
+++ stats.py
@@ -1,2 +1,8 @@
 def count_words(text):
-    return len(text.split())
+         return len(text.split())
+
+def count_characters(text):
+    s = text.lower()
+    from collections import Counter
+    counts = Counter(s)
+    return dict(counts)
